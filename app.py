b02513d70050f902cb74a43bab980c5fb8a17faa
Dan-data-tech <diab.danawork@gmail.com>
2025-03-03 11:34:07 +0200

    added icons on the starters

--- app.py
+++ app.py
@@ -30,23 +30,23 @@ async def set_starters():
         cl.Starter(
             label="Morning routine ideation",
             message="Can you help me create a personalized morning routine that would help increase my productivity throughout the day? Start by asking me about my current habits and what activities energize me in the morning.",
-            icon=r"assets\lightbulb-on.png",
+            icon=r"public\idea.svg",
             ),
 
         cl.Starter(
             label="Explain superconductors",
             message="Explain superconductors like I'm five years old.",
-            icon=r"/public/learn.svg",
+            icon=r"public\learn.svg",
             ),
         cl.Starter(
             label="Python script for daily email reports",
             message="Write a script to automate sending daily email reports in Python, and walk me through how I would set it up.",
-           # icon="/public/terminal.svg",
+            icon=r"public\code.svg",
             ),
         cl.Starter(
             label="Text inviting friend to wedding",
             message="Write a text asking a friend to be my plus-one at a wedding next month. I want to keep it super short and casual, and offer an out.",
-            #icon="/public/write.svg",
+            icon=r"public\write.svg",
             )
         ]
 
